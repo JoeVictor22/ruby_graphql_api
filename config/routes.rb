a5b270181771f6195d04d97d37f3bc34fc99e139
Joel <joelvictor1746@gmail.com>
2022-06-12 22:59:27 -0300

    feat: testing changes on routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
-  resources :bands do
-    resources :members
-  end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api do
+    namespace :v1 do
+      resources :bands do
+        resources :members
+      end
+    end
+    namespace :v2 do
+      resources :members
+      resources :bands
+    end
+  end
+
 end
